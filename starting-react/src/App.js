53ee6b5aa50b07efbee580d917bc12fd48ecf4be
Aaron N <Aaron-N@users.noreply.github.com>
2024-09-02 10:35:13 -0700

    final practice changes pt 2

--- starting-react/src/App.js
+++ starting-react/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 import './App.css';
-import pokemon from './pokemon.json';
+import { Button } from '@mui/material'
 
-const PokemonRow = ({ pokemon }) => {
+const PokemonRow = ({ pokemon, onSelect }) => {
   return (
     <tr>
       <td>{pokemon.name.english}</td>
       <td>{pokemon.type.join(", ") }</td>
+      <td>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => onSelect(pokemon)}
+        >Select!</Button>
+      </td>
     </tr>
   )
 }
@@ -15,37 +23,103 @@ const PokemonRow = ({ pokemon }) => {
 PokemonRow.propTypes = {
   pokemon: PropTypes.shape({
     name: PropTypes.shape({
-      english: PropTypes.string
+      english: PropTypes.string.isRequired
     }),
-    type: PropTypes.arrayOf(PropTypes.string)
-  })
+    type: PropTypes.arrayOf(PropTypes.string.isRequired)
+  }),
+  onSelect: PropTypes.func.isRequired
 }
 
-function App() {
-  return (
-    <div 
-      style={{ 
-        margin: "auto",
-        width: 800,
-        paddingTop: "1rem"
-      }}
-    >
-      <h1 className='title'>Pokemon Search</h1>
-      <table width="100%">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
+const PokemonInfo = ({ name, base }) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      {Object.keys(base).map((key) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{base[key]}</td>
           </tr>
-        </thead>
-        <tbody>
-          {pokemon.slice(0, 20).map((pokemon) => (
-            <PokemonRow pokemon={pokemon} key={pokemon.id} />
-          ))}
-          
-        </tbody>
-      </table>
-    </div>
+      ))}
+    </table>
+  </div>
+)
+
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({
+    english: PropTypes.string.isRequired
+  }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defense": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired
+  })
+};
+
+const Title = styled.h1`
+  text-align: center;
+`;
+const TwoColumnLayout = styled.div`
+  display: grid,
+  grid-template-columns: 70% 30%,
+  grid-column-gap: 1rem
+`;
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1rem;
+`;
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+
+function App() {
+  const [filter, setFilter] = React.useState("");
+  const [pokemon, setPokemon] = React.useState([]);
+  const [selectedItem, setSelectedItem] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/starting-react/pokemon.json")
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  }, []);
+
+  return (
+    <Container>
+      <Title>Pokemon Search</Title>
+      <Input 
+        value={(filter)}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <TwoColumnLayout> 
+        <div>
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon
+                .filter((pokemon) => 
+                  pokemon.name.english.toLowerCase().includes(filter.toLowerCase()))
+                .slice(0, 20)
+                .map((pokemon) => (
+                  <PokemonRow pokemon={pokemon} key={pokemon.id} onSelect={(pokemon) => setSelectedItem(pokemon)}/>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && (
+          <PokemonInfo {...selectedItem} />
+        )}
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
